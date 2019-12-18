b4a4443191deff3a32834bfced72151c977ad1ff
Nicola Verbeeck <nicola@chimerapps.com>
2019-12-18 16:30:55 +0100

    Version bump Hack name of missing field

--- detekt/src/main/kotlin/com/icapps/swaggervalidator/SwaggerValidatorRule.kt
+++ detekt/src/main/kotlin/com/icapps/swaggervalidator/SwaggerValidatorRule.kt
@@ -20,23 +20,10 @@ import com.icapps.swaggervalidator.model.PostValidationHelper
 import com.icapps.swaggervalidator.parser.SwaggerDownloader
 import com.icapps.swaggervalidator.parser.SwaggerProperty
 import com.icapps.swaggervalidator.parser.SwaggerPropertyTypes
-import io.gitlab.arturbosch.detekt.api.CodeSmell
-import io.gitlab.arturbosch.detekt.api.Debt
-import io.gitlab.arturbosch.detekt.api.Entity
-import io.gitlab.arturbosch.detekt.api.Issue
-import io.gitlab.arturbosch.detekt.api.Rule
-import io.gitlab.arturbosch.detekt.api.Severity
+import io.gitlab.arturbosch.detekt.api.*
 import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.com.intellij.psi.util.PsiTreeUtil
-import org.jetbrains.kotlin.psi.KtAnnotationEntry
-import org.jetbrains.kotlin.psi.KtClass
-import org.jetbrains.kotlin.psi.KtEnumEntry
-import org.jetbrains.kotlin.psi.KtLiteralStringTemplateEntry
-import org.jetbrains.kotlin.psi.KtNullableType
-import org.jetbrains.kotlin.psi.KtPrimaryConstructor
-import org.jetbrains.kotlin.psi.KtStringTemplateExpression
-import org.jetbrains.kotlin.psi.KtTypeElement
-import org.jetbrains.kotlin.psi.KtUserType
+import org.jetbrains.kotlin.psi.*
 
 class SwaggerValidatorRule() : Rule() {
 
@@ -115,7 +102,8 @@ class SwaggerValidatorRule() : Rule() {
 
             val modelConstructorParameters = getModelConstructorParameters(constructor)
 
-            val swaggerDefinition = SwaggerDownloader.instance(config = ruleSetConfig).getFuture().get().definitions.find { it.name == swaggerModelName }
+            val swaggerDefinition = SwaggerDownloader.instance(config = ruleSetConfig).getFuture().get()
+                .definitions.find { it.name == swaggerModelName }
             if (swaggerDefinition == null) {
                 report(
                     CodeSmell(
@@ -128,12 +116,13 @@ class SwaggerValidatorRule() : Rule() {
             }
             //TODO what about defined fields that do not exist in swagger!
             swaggerDefinition.properties.forEach { swaggerProperty ->
-                val modelParameter = modelConstructorParameters.find { parameter -> parameter.name == swaggerProperty.name }
+                val modelParameter =
+                    modelConstructorParameters.find { parameter -> parameter.name == swaggerProperty.name }
                 if (modelParameter == null) {
                     report(
                         CodeSmell(
                             swaggerFieldNotFound,
-                            Entity.from(klass),
+                            buildNamedEntity(klass, "${swaggerProperty.name}"),
                             "'${swaggerProperty.name}' property defined in swagger not found in model!"
                         )
                     )
@@ -145,7 +134,7 @@ class SwaggerValidatorRule() : Rule() {
                 if (swaggerDefinition.properties.none { swaggerProperty -> swaggerProperty.name == modelParameter.name }) {
                     report(
                         CodeSmell(
-                            swaggerFieldNotFound,
+                            noSuchSwaggerField,
                             Entity.from(modelParameter.element),
                             "'${modelParameter.name}' property defined in model but not in swagger!"
                         )
@@ -215,27 +204,60 @@ class SwaggerValidatorRule() : Rule() {
                         )
                     )
                 } else {
-                    PostValidationHelper.markRequiredEnum(modelParameter.type.name, this, modelParameter.element, swaggerProperty.type.enumValues)
+                    PostValidationHelper.markRequiredEnum(
+                        modelParameter.type.name,
+                        this,
+                        modelParameter.element,
+                        swaggerProperty.type.enumValues
+                    )
                 }
             } else {
                 when (swaggerProperty.type.format) {
-                    "date-time", "date" -> reportTypeMismatch(modelParameter.element, listOf("String", "Date", "Calendar"), modelParameter.type.name)
-                    "byte" -> reportTypeMismatch(modelParameter.element, listOf("String", "ByteArray"), modelParameter.type.name)
+                    "date-time", "date" -> reportTypeMismatch(
+                        modelParameter.element,
+                        listOf("String", "Date", "Calendar"),
+                        modelParameter.type.name
+                    )
+                    "byte" -> reportTypeMismatch(
+                        modelParameter.element,
+                        listOf("String", "ByteArray"),
+                        modelParameter.type.name
+                    )
                     else -> reportTypeMismatch(modelParameter.element, "String", modelParameter.type.name)
                 }
             }
             SwaggerPropertyTypes.NUMBER -> when (swaggerProperty.type.format) {
-                "integer", "int32" -> reportTypeMismatch(modelParameter.element, listOf("Int", "Long"), modelParameter.type.name)
+                "integer", "int32" -> reportTypeMismatch(
+                    modelParameter.element,
+                    listOf("Int", "Long"),
+                    modelParameter.type.name
+                )
                 "int64" -> reportTypeMismatch(modelParameter.element, "Long", modelParameter.type.name)
-                "double", "float" -> reportTypeMismatch(modelParameter.element, listOf("Float", "Double"), modelParameter.type.name)
+                "double", "float" -> reportTypeMismatch(
+                    modelParameter.element,
+                    listOf("Float", "Double"),
+                    modelParameter.type.name
+                )
                 else -> reportTypeMismatch(modelParameter.element, "Number", modelParameter.type.name)
             }
             SwaggerPropertyTypes.INTEGER -> when (swaggerProperty.type.format) {
                 "int64" -> reportTypeMismatch(modelParameter.element, "Long", modelParameter.type.name)
-                "integer", "int32", null -> reportTypeMismatch(modelParameter.element, listOf("Int", "Long"), modelParameter.type.name)
-                else -> reportTypeMismatch(modelParameter.element, "<error, unknown swagger format for integer>", modelParameter.type.name)
+                "integer", "int32", null -> reportTypeMismatch(
+                    modelParameter.element,
+                    listOf("Int", "Long"),
+                    modelParameter.type.name
+                )
+                else -> reportTypeMismatch(
+                    modelParameter.element,
+                    "<error, unknown swagger format for integer>",
+                    modelParameter.type.name
+                )
             }
-            SwaggerPropertyTypes.BOOLEAN -> reportTypeMismatch(modelParameter.element, "Boolean", modelParameter.type.name)
+            SwaggerPropertyTypes.BOOLEAN -> reportTypeMismatch(
+                modelParameter.element,
+                "Boolean",
+                modelParameter.type.name
+            )
             SwaggerPropertyTypes.ARRAY -> report(
                 CodeSmell(
                     typeMismatch,
@@ -244,7 +266,11 @@ class SwaggerValidatorRule() : Rule() {
                 )
             )
             SwaggerPropertyTypes.REF -> {
-                PostValidationHelper.markRequiredDeclarationValidation(swaggerProperty.type.referredType!!, this, modelParameter.element)
+                PostValidationHelper.markRequiredDeclarationValidation(
+                    swaggerProperty.type.referredType!!,
+                    this,
+                    modelParameter.element
+                )
             }
         }
     }
@@ -283,10 +309,15 @@ class SwaggerValidatorRule() : Rule() {
 
             val type = Type.create(typeElement)
 
-            val jsonAnnotation = parameter.annotationEntries.find { annotation -> annotation.shortName?.asString() == "Json" }
+            val jsonAnnotation =
+                parameter.annotationEntries.find { annotation -> annotation.shortName?.asString() == "Json" }
             val overrideName = jsonAnnotation?.let { overrideAnnotation ->
                 extractStringFieldFromAnnotation(overrideAnnotation, "name") {
-                    throw IllegalStateException("Got json annotation but not name field! (${Entity.from(overrideAnnotation.navigationElement).compact()})")
+                    throw IllegalStateException(
+                        "Got json annotation but not name field! (${Entity.from(
+                            overrideAnnotation.navigationElement
+                        ).compact()})"
+                    )
                 }
             }
             ConstructorParameter(overrideName ?: parameter.name!!, type, typeElement.navigationElement)
@@ -334,7 +365,12 @@ data class Type(val name: String, val isNullable: Boolean, val isGeneric: Boolea
 
             //Special case for List<String> which can be parceled
             if (name == "ParcelableStringList") {
-                return Type("List", isNullable, true, listOf(Type("String", isNullable = false, isGeneric = false, genericParameters = emptyList())))
+                return Type(
+                    "List",
+                    isNullable,
+                    true,
+                    listOf(Type("String", isNullable = false, isGeneric = false, genericParameters = emptyList()))
+                )
             }
 
             return Type(name, isNullable, isGeneric, genericTypes.map { create(it.typeElement!!) })
@@ -343,7 +379,11 @@ data class Type(val name: String, val isNullable: Boolean, val isGeneric: Boolea
     }
 }
 
-private fun extractStringFieldFromAnnotation(annotation: KtAnnotationEntry, annotationFieldName: String, messageIfNotString: () -> Unit): String? {
+private fun extractStringFieldFromAnnotation(
+    annotation: KtAnnotationEntry,
+    annotationFieldName: String,
+    messageIfNotString: () -> Unit
+): String? {
     val name = annotation.valueArguments.find { it.getArgumentName()?.asName?.asString() == annotationFieldName }
 
     val expression = name?.getArgumentExpression() as? KtStringTemplateExpression
@@ -356,4 +396,8 @@ private fun extractStringFieldFromAnnotation(annotation: KtAnnotationEntry, anno
         messageIfNotString()
     }
     return stringField
+}
+
+private fun buildNamedEntity(source: PsiElement, overrideName: String): Entity {
+    return Entity.from(source).copy(name = overrideName)
 }
